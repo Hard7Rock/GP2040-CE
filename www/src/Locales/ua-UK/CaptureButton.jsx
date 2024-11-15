212f78536a143b20eebaf76421e0380feb496790
hard <hard.rock.melnyk@gmail.com>
2024-11-15 17:15:54 +0200

    2nd

---
+++ www/src/Locales/ua-UK/CaptureButton.jsx
@@ -0,0 +1,7 @@
+export default {
+        'capture-button-button-label': 'Призначити кнопки за допомогою',
+        'capture-button-modal-title': 'Очікується натискання кнопки',
+        'capture-button-modal-content': 'Натисніть кнопку на геймпаді.',
+        'capture-button-modal-skip': 'Пропустити кнопку',
+        'capture-button-modal-stop': 'Зупинити захоплення',
+};
