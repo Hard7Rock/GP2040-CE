92f8f937e73eac1f955d8dfb83ae4391c9baddf7
hard <hard.rock.melnyk@gmail.com>
2024-11-15 17:12:09 +0200

    begin translation

---
+++ www/src/Locales/ua-UK/BackupPage.jsx
@@ -0,0 +1,18 @@
+export default {
+	'pin-version-warning-text':
+		'Імпорт прив’язки пінів із файлів до версії v0.7.6 не має ефекту.',
+	'saved-success-message': 'Збережено як: {{name}}',
+	'save-header-text': 'Резервне копіювання у файл',
+	'save-export-option-label': 'Експортувати параметри {{api}}',
+	'load-header-text': 'Відновлення з файлу',
+	'load-export-option-label': 'Імпортувати параметри {{api}}',
+	'api-display-text': 'Дисплей',
+	'api-gamepad-text': 'Геймпад',
+	'api-led-text': 'LED',
+	'api-ledTheme-text': 'Користувацька LED тема',
+	'api-macros-text': 'Прив’язка макросів',
+	'api-pins-text': 'Прив’язка пінів',
+	'api-profiles-text': 'Прив’язка профілів',
+	'api-addons-text': 'Додатки',
+	'api-splash-text': 'Зображення стартового екрана',
+};
